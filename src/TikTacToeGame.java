7d8993676f41ce8f670c11988d322259a07abd1e
chetan <zambare.c123@gmail.com>
2020-11-21 19:08:50 +0530

    Add board with size 10

--- src/TikTacToeGame.java
+++ src/TikTacToeGame.java
@@ -1,13 +1,11 @@
 import java.util.Scanner;
 
 public class TikTacToeGame {
-	//static char [] Board = new char[10];
+	static char [] Board = new char[10];
 	
 	public static void main(String[] args) {
 		System.out.println(" Start game");
 		
-		
-
 	}
 	
 
