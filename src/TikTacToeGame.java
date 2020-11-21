6d499f9c176ad93d12fdbf69bb2707fd296178c7
chetan <zambare.c123@gmail.com>
2020-11-21 19:10:48 +0530

    Ask Player what he want to take X or O

--- src/TikTacToeGame.java
+++ src/TikTacToeGame.java
@@ -4,9 +4,15 @@ public class TikTacToeGame {
 	static char [] Board = new char[10];
 	
 	public static void main(String[] args) {
-		System.out.println(" Start game");
+		chooseMove();
 		
 	}
-	
+	public static char chooseMove() {
+		Scanner sc1 = new Scanner(System.in);
+		System.out.println("Chose what player want X or O Please mention");
+		String mark1 = sc1.next();
+		char mark = mark1.charAt(0);
+		return mark;
+	}
 
 }
