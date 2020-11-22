4003aec423a308e9ac695f22a780bf7f29360edd
chetan <zambare.c123@gmail.com>
2020-11-22 11:15:46 +0530

    Take input X or O for Player

--- src/TikTacToeGame.java
+++ src/TikTacToeGame.java
@@ -2,17 +2,34 @@ import java.util.Scanner;
 
 public class TikTacToeGame {
 	static char [] Board = new char[10];
+	static char userMark ;
+	static char computerMark;
+	 
 	
 	public static void main(String[] args) {
-		chooseMove();
+		chooseMark();
+		System.out.println("UserMark " + userMark + " & " + " ComputerMark " + computerMark);
 		
 	}
-	public static char chooseMove() {
+	
+	public static void chooseMark() {
 		Scanner sc1 = new Scanner(System.in);
-		System.out.println("Chose what player want X or O Please mention");
-		String mark1 = sc1.next();
-		char mark = mark1.charAt(0);
-		return mark;
+		while(true) {  ///If person provide proper input loop will break
+			System.out.println("Chose what player want X or O Please mention");
+			String mark1 = sc1.next();
+			userMark = mark1.charAt(0);
+			if (mark1.equals("X")) {
+				computerMark = "O".charAt(0);
+				break;
+			}
+			else if (mark1.equals("O")) {
+				computerMark = "X".charAt(0);
+				break;
+			}
+			else {
+				System.out.println("Wrong Input");
+			}
+		}
 	}
 
 }
