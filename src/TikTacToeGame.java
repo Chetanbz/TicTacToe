744983c990433ca26ab65c297ee3d6149f40cf72
chetan <zambare.c123@gmail.com>
2020-11-22 12:55:06 +0530

    Toss is thrown, Decide first play status

--- src/TikTacToeGame.java
+++ src/TikTacToeGame.java
@@ -4,22 +4,32 @@ public class TikTacToeGame {
 	static char [] Board = new char[10];
 	static char userMark ;
 	static char computerMark;
+	static int tossNumber;
 	 
 	
 	public static void main(String[] args) {
-		Board[5] = "X".charAt(0);  /// Assume this is old history
 		TikTacToeGame Game = new TikTacToeGame();
+		tossNumber = toss();
 		Game.chooseMark();
 		System.out.println(Game.showBoard());
-		Game.userMove(userMark);
-		System.out.println(Game.showBoard());
+		if (tossNumber ==0) {
+			Game.computerMove();
+			System.out.println(Game.showBoard());
+		}
+		else {
+			Game.userMove();
+			System.out.println(Game.showBoard());
+		}
 	}
 	
 	public  void chooseMark() {
 		Scanner sc1 = new Scanner(System.in);
+		String mark1 ="O";
 		while(true) {  ///If person provide proper input loop will break
-			System.out.println("Chose what player want X or O Please mention");
-			String mark1 = sc1.next();
+			if (tossNumber == 1) {
+				System.out.println("Chose what player want X or O Please mention");
+				mark1 = sc1.next();
+			}
 			userMark = mark1.charAt(0);
 			if (mark1.equals("X")) {
 				computerMark = "O".charAt(0);
@@ -31,26 +41,47 @@ public class TikTacToeGame {
 			}
 			else {
 				System.out.println("Wrong Input");
-			}
+			}	
 		}
 	}
 	public String showBoard() {
 		System.out.println("Current Board Position is ");
 	      return  Board[1] + " | " + Board[2] + " | " + Board[3] +"\n" +  Board[4] + " | " + Board[5] + " | " + Board[6] + "\n" +  Board[7] + " | " + Board[8] + " | " + Board[9];
 	}
+	public static int toss() {
+		int outcome = (int)Math.floor(Math.random()*10)%2;
+		if (outcome ==1) {
+			System.out.println(" Player won toss");
+		}
+		else {
+			System.out.println(" Computer won toss");
+		}
+		return outcome;
+	}
 
-	public void userMove(char playerMark) {
+	public void userMove() {
 		Scanner sc1 = new Scanner(System.in);
 		while(true) {
 			System.out.println("Choose number to place Mark");
 			int moveNumber = sc1.nextInt(); ///which place user wants to put his marks
 			String position = String.valueOf(Board[moveNumber]); 
 			if (!(position.equals("X") || position.equals("O"))) {  // check for empty space & loop breaks
-				Board[moveNumber] = playerMark;
+				Board[moveNumber] = userMark;
 				break;
 			}
 		}
 	}
-
+	
+	public void computerMove() {
+		while(true) {
+			int computerNumber = (int)Math.floor(Math.random()*10)%9 + 1;
+			System.out.println("Computer chooses " + computerNumber);
+			String position = String.valueOf(Board[computerNumber]); 
+			if (!(position.equals("X") || position.equals("O"))) {  // check for empty space & loop breaks
+				Board[computerNumber] = computerMark;
+				break;
+			}
+		}
+	}
 
 }
